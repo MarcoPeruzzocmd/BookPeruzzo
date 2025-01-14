3ebcc1fb31345244734bb25b277242d011d7216e
marcoperuzzzocmd <marcoantperuzzo@gmail.com>
2025-01-14 13:40:23 -0300

    feat

--- src/components/HomeComp/HomeVue.vue
+++ src/components/HomeComp/HomeVue.vue
@@ -6,21 +6,37 @@
         <img class="search" src="/src/assets/search.png" alt="">
     </nav>
     <div class="content">
-        
+        <section class="books">
+            <h1 class="tittleEmphasis">Best Deals</h1>
+            <div class="booksCarroussels">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </section>
+        <section class="books">
+
+        </section>
+        <section class="books">
+
+        </section>
     </div>
 </template>
 
 <style scoped>
-nav{
+nav {
     display: flex;
-    justify-content:space-between;
+    justify-content: space-between;
     margin: 18px;
 }
-.happyMessage{
+
+.happyMessage {
     font-size: 16px;
     font-family: "Open Sans", serif;
 }
-.search{
+
+.search {
     height: 32px;
     width: 32px;
     cursor: pointer;
